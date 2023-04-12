c39396bef92c12715da626f8efc240bfd5c33f3e
rishanaa <rishanaihthi@gmail.com>
2023-04-12 13:07:21 +0530

    home

---
+++ src/Home/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div><h5>Countries</h5></div>
+      <div>
+        <h5>All</h5>
+        <h5>Asia</h5>
+        <h5>Europe</h5>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
